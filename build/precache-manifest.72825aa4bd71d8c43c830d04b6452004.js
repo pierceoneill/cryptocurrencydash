ac29f26302bc82fdf2c295b52b4bc6416bf346ea
Pierce O'Neill <pierceoneillky@gmail.com>
2019-05-21 22:02:22 +0100

    initial commit

---
+++ build/precache-manifest.72825aa4bd71d8c43c830d04b6452004.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "da2e6b441e1b4c6e6c4e",
+    "url": "/static/js/main.da2e6b44.chunk.js"
+  },
+  {
+    "revision": "a256425ff2c782af36ec",
+    "url": "/static/js/1.a256425f.chunk.js"
+  },
+  {
+    "revision": "da2e6b441e1b4c6e6c4e",
+    "url": "/static/css/main.24d80c74.chunk.css"
+  },
+  {
+    "revision": "77098db1ea2bb8d7b64cc766eaed3f1f",
+    "url": "/index.html"
+  }
+];
